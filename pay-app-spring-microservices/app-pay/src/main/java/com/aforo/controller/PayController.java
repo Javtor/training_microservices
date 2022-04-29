f2543ea9621a94b9f095938eadffbba3c9d0ceb8
Javier Torres <javierandrestorresreyes@gmail.com>
2022-04-29 13:33:21 -0500

    changes in properties for kubernetes

--- pay-app-spring-microservices/app-pay/src/main/java/com/aforo/controller/PayController.java
+++ pay-app-spring-microservices/app-pay/src/main/java/com/aforo/controller/PayController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +33,10 @@ public class PayController {
         producer.sendPayEvent(respose);
         log.info("Mensaje agregado a la cola correctamente");
         return ResponseEntity.status(HttpStatus.CREATED).body(respose);
+    }
+
+    @GetMapping("/test")
+    public String postDepositEvent() {
+        return "pay";
     }
 }
